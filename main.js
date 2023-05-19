0b67ce0bcf9b74bed21b13dd589120b325568642
ilyas182 <ilyas2104@gmail.com>
2023-05-19 17:42:03 +0800

    GA project 1

--- main.js
+++ main.js
@@ -39,6 +39,8 @@ const gameScreen = document.querySelector("#gameScreen");
 const startButton = document.querySelector("#startButton");
 
 let tries = document.querySelector('#tries');
+let secretWord;
+let letter;
 /*----- event listeners -----*/
 startButton.addEventListener("click", handleStart);
 
@@ -50,10 +52,53 @@ function renderScreen() {
   
   document.querySelector(`#${game.screen}`).classList.remove("hide");
 }
+function renderTry() {
+  document.querySelector('#tries').innerText = "Tries left:" + game.triesLeft;
+}
+
+
 
 //!!!!!!!!!!!!!!!!! CONTROLLER (logic) !!!!!!!!!!!!!!!!!!!
 
 function handleStart() {
   game.screen = "gameScreen";
+  secretWord = game.secretWord(); 
+  console.log(secretWord);
   renderScreen();
+  renderTry();
+  createWord();
+  keypressEvent();
+}
+
+function createWord() {
+  for (let i = 0; i < secretWord.length; i++)
+  {
+    letter = document.createElement("h3");
+    letter.innerText = secretWord.charAt(i);
+    console.log(letter);
+    gameScreen.appendChild(letter);
+    letter.classList.add("horizontal");
+    letter.classList.add("hide-text");
+    letter.setAttribute('id', i);
+    
+  }  
+}
+
+function keypressEvent() {
+  document.addEventListener('keypress', function(event) {
+    
+      console.log(event.key);
+      for (let i = 0; i < secretWord.length; i++){
+        if (event.key == secretWord.charAt(i).toLowerCase())
+        {
+          document.getElementById(`${i}`).classList.remove('hide-text');            
+        }
+        else
+        {
+        game.triesLeft--;
+        console.log(game.triesLeft);
+        renderTry;
+        }
+      }
+      })
 }
